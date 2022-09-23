ced7d70269938b038f3d35413038042a935b878b
DartNihil <104585286+DartNihil@users.noreply.github.com>
2022-09-24 00:25:46 +0300

    Add Entity

---
+++ src/main/java/by/tms/entity/User.java
@@ -0,0 +1,17 @@
+package by.tms.entity;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class User extends Entity {
+    @NotBlank(message = "Field must not be empty")
+    @Email
+    String email;
+
+    @NotBlank(message = "Field must not be empty")
+    @Size(message = "Password length must be 5-10 characters!", min = 5, max = 10)
+    @Pattern(message = "Password must consist of numbers and latin letters!", regexp = "[\\w\\d]+")
+    String password;
+}
