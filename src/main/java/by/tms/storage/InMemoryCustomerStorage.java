28bc22429e5e5c0c24c863c3e6dc5a937e27ab1b
ValentinRadukevich <valentin.radukevih@gmail.com>
2022-09-26 21:31:59 +0300

    Add Storage

---
+++ src/main/java/by/tms/storage/InMemoryCustomerStorage.java
@@ -0,0 +1,49 @@
+package by.tms.storage;
+
+import by.tms.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Component("inMemoryCustomerStorage")
+public class InMemoryCustomerStorage implements Storage<Customer, Long> {
+    @Autowired
+    private List<Customer> customerList;
+    @Autowired
+    private AtomicLong CustomerIdGenerator;
+
+    @Override
+    public Customer save(Customer customer) {
+        customer.setId(customerIdGenerator.incrementAndGet());
+        customerList.add(customer);
+        return customer;
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id) {
+                return Optional.of(customer);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Customer> getListOfEntity() {
+        return customerList;
+    }
+
+    @Override
+    public Optional<Customer> findEntity(String email) {
+        for (Customer customer : customerList) {
+            if (customer.getEmail().equals(email)) {
+                return Optional.of(customer);
+            }
+        }
+        return Optional.empty();
+    }
+}
