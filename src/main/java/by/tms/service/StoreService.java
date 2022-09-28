ee4050eb51bd6c5b41a2886306330c886d4bceb4
Stonewall1 <impression97p@gmail.com>
2022-09-28 22:12:04 +0300

    findStore method added + customer login added in postmapping

--- src/main/java/by/tms/service/StoreService.java
+++ src/main/java/by/tms/service/StoreService.java
@@ -1,10 +1,13 @@
 package by.tms.service;
 
+import by.tms.entity.Customer;
 import by.tms.entity.Store;
 import by.tms.storage.Storage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class StoreService {
@@ -15,5 +18,8 @@ public class StoreService {
     public Store saveStore(Store store) {
         storeStorage.save(store);
         return store;
+    }
+    public Optional<Store> findStoreByEmail(String email){
+        return storeStorage.findEntity(email);
     }
 }
