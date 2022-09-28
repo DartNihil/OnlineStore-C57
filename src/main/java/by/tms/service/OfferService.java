5fbf7a816c58e7f6f5559ad37658ff6eb40dd688
Anastasia <a.a.pligina@gmail.com>
2022-09-28 11:33:44 +0300

    add service method createOfferWithStoreAndProductCategory

--- src/main/java/by/tms/service/OfferService.java
+++ src/main/java/by/tms/service/OfferService.java
@@ -1,11 +1,13 @@
 package by.tms.service;
 
-import by.tms.entity.Offer;
-import by.tms.entity.Store;
+import by.tms.entity.*;
 import by.tms.storage.Storage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class OfferService {
@@ -18,6 +20,21 @@ public class OfferService {
         offerStorage.save(offer);
         System.out.println(offer.getProduct().getClass().toString());
         System.out.println(offer);
+        return offer;
+    }
+    public Offer createOfferWithStoreAndProductCategory(Offer offer, Store store, String productCategory) {
+        offer.setStore(store);
+        Map<String, Product> productMap = new HashMap<>();
+        productMap.put("Smartphone", new Smartphone());
+        productMap.put("Notebook", new Notebook());
+        productMap.put("ElectronicBook", new ElectronicBook());
+        productMap.put("Smartwatch", new Smartwatch());
+        productMap.put("Tablet", new Tablet());
+        for (String key: productMap.keySet()) {
+            if (key.equals(productCategory)) {
+                offer.setProduct(productMap.get(key));
+            }
+        }
         return offer;
     }
 }
