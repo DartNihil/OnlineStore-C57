0dd5b6de684442e50441fd89e409b4bdbbb9e001
Anastasia <a.a.pligina@gmail.com>
2022-09-28 16:08:35 +0300

    add validation of OfferDTO

--- src/main/java/by/tms/dto/OfferDTO.java
+++ src/main/java/by/tms/dto/OfferDTO.java
@@ -1,9 +1,16 @@
 package by.tms.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 public class OfferDTO {
+    @NotBlank(message = "Field must not be empty")
+    @Size(message = "Description length must be more then 2", min = 2)
     private String description;
+    @NotBlank(message = "Field must not be empty")
+    @Pattern(message = "Must be numbers", regexp = "\\d+")
     private BigDecimal price;
 
     public OfferDTO(String description, BigDecimal price) {
