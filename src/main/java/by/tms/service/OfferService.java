45b7844d8850df476af9da4c24f12b0755fea0f9
a.a.pligina <a.a.pligina@gmail.com>
2022-10-07 20:52:43 +0300

    add methods in OfferService

--- src/main/java/by/tms/service/OfferService.java
+++ src/main/java/by/tms/service/OfferService.java
@@ -1,13 +1,17 @@
 package by.tms.service;
 
+import by.tms.composite.OfferComposite;
 import by.tms.entity.*;
 import by.tms.storage.Storage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class OfferService {
@@ -25,5 +29,78 @@ public class OfferService {
         offer.setStore(store);
         offer.setProduct(product);
         return offer;
+    }
+    public Optional<Offer> findOfferById(long id) {
+        return offerStorage.findById(id);
+    }
+
+    public List<OfferComposite> addOfferToCart(long offerId, Customer customer) {
+
+        List<OfferComposite> cart = customer.getCart();
+        boolean isOfferInCart = false;
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getOffer().getId() == offerId) {
+                isOfferInCart = true;
+                int count = cart.get(i).getCount();
+                cart.get(i).setCount(count + 1);
+                break;
+            }
+        }
+        if (!isOfferInCart) {
+            Optional<Offer> offerInBase = findOfferById(offerId);
+            Offer offer = null;
+            if (offerInBase.isPresent()) {
+                offer = offerInBase.get();
+            }
+            OfferComposite offerComposite = new OfferComposite(offer, 1);
+            cart.add(offerComposite);
+        }
+        return cart;
+    }
+
+    public BigDecimal findTotalPriceOffersInCart(List<OfferComposite> cart) {
+        BigDecimal totalPrice = new BigDecimal(0);
+        for (int i = 0; i < cart.size(); i++) {
+            BigDecimal offerCompositePrice = cart.get(i).getOffer().getPrice().multiply(new BigDecimal(cart.get(i).getCount()));
+            totalPrice = totalPrice.add(offerCompositePrice);
+        }
+        return totalPrice;
+    }
+
+    public List<OfferComposite> deleteOfferFromCart(long offerId, Customer customer) {
+        List<OfferComposite> cart = customer.getCart();
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getOffer().getId() == offerId) {
+                cart.remove(i);
+                break;
+            }
+        }
+        return cart;
+    }
+
+    public List<OfferComposite> minusOfferCount(long offerId, Customer customer) {
+        List<OfferComposite> cart = customer.getCart();
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getOffer().getId() == offerId) {
+                int count = cart.get(i).getCount();
+                if (count > 1) {
+                    cart.get(i).setCount(count - 1);
+                }
+                break;
+            }
+        }
+        return cart;
+    }
+
+    public List<OfferComposite> plusOfferCount(long offerId, Customer customer) {
+        List<OfferComposite> cart = customer.getCart();
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getOffer().getId() == offerId) {
+                int count = cart.get(i).getCount();
+                cart.get(i).setCount(count + 1);
+                break;
+            }
+        }
+        return cart;
     }
 }
