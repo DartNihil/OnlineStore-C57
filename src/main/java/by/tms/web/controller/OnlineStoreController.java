38a8b8796e8a48b98945abfc7d12b6d6bb1e7470
Stonewall1 <impression97p@gmail.com>
2022-10-07 22:19:14 +0300

    storeProfile get-post mappings added

--- src/main/java/by/tms/web/controller/OnlineStoreController.java
+++ src/main/java/by/tms/web/controller/OnlineStoreController.java
@@ -1,13 +1,16 @@
 package by.tms.web.controller;
 
+import by.tms.entity.Customer;
 import by.tms.entity.Store;
 import by.tms.service.CustomerService;
+import by.tms.service.OfferService;
 import by.tms.service.StoreService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
@@ -15,10 +18,12 @@ import javax.validation.Valid;
 public class OnlineStoreController {
     private final StoreService storeService;
     private final CustomerService customerService;
+    private final OfferService offerService;
 
-    public OnlineStoreController(StoreService storeService, CustomerService customerService) {
+    public OnlineStoreController(StoreService storeService, CustomerService customerService, OfferService offerService) {
         this.storeService = storeService;
         this.customerService = customerService;
+        this.offerService = offerService;
     }
 
     @GetMapping("/storeRegistration")
@@ -39,5 +44,20 @@ public class OnlineStoreController {
             model.addAttribute("message", "User already exists");
             return "storeRegistration";
         }
+    }
+    @GetMapping("/storeProfile")
+    public String storeProfile(HttpSession session, Model model) {
+        if (session.getAttribute("currentUser") instanceof Customer) {
+            return "redirect:/"; //should return "customer/customerProfile"
+        } else if (session.getAttribute("currentUser") instanceof Store) {
+            model.addAttribute("listOfOffers" , offerService.findOffersByStore((Store) session.getAttribute("currentUser")));
+            return "storeProfile";
+        }
+        return "redirect:/login";
+    }
+
+    @PostMapping("/storeProfile")
+    public String storeProfile() {
+        return "storeProfile";
     }
 }
