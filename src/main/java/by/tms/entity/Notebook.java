80742bc9411f848bb900bb81e5e2a326b1de7118
a.a.pligina <87080361+AnastasiaPlg@users.noreply.github.com>
2022-10-09 21:20:04 +0300

    Update Notebook.java

--- src/main/java/by/tms/entity/Notebook.java
+++ src/main/java/by/tms/entity/Notebook.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.Size;
 public class Notebook extends AbstractProduct {
     @NotBlank(message = "Field must not be empty")
     @Size(message = "Hard disk drive type length must be more then 2", min = 2)
-
     private String hardDiskDriveType;
 
     public Notebook() {
